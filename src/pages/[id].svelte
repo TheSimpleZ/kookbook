1f91472c47c186ae0abc88531b62c2070d36a87f
Zrean Tofiq <mote93@gmail.com>
2021-03-10 22:37:17 +0100

    Fix styling

--- src/pages/[id].svelte
+++ src/pages/[id].svelte
@@ -52,36 +52,36 @@
     recipeRef = e.detail.ref
   }}
 >
-  <Header>
-    <div class="flex items-center justify-center flex-1">
-      <h1 class="mx-2 text-3xl font-bold">{recipe.name}</h1>
-      <button on:click={openRenameDialog}><Icon icon={pencil} class="w-4 h-4 m-1" /></button>
-    </div>
-  </Header>
-
-  <Dialog visible={showRenameDialog} inverted closable={false}>
-    <div class="flex flex-col h-40 p-5">
-      <label for="recipe_name" class="block text-sm font-medium text-gray-700">Name</label>
-      <input
-        id="recipe_name"
-        type="text"
-        bind:value={recipeNameInput}
-        class="mt-0 block w-60 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"
-      />
-      <div class="flex justify-end gap-4 mt-auto">
-        <button class="text-white bg-red-500 dialogBtn hover:bg-red-400" on:click={closeDialog}>Cancel</button>
-        <button
-          class="text-indigo-500 bg-transparent dialogBtn hover:bg-gray-100 hover:text-indigo-400"
-          on:click={saveName}
-        >
-          Save
-        </button>
+  <main class="flex flex-col min-h-screen">
+    <Header>
+      <div class="flex items-center justify-center flex-1">
+        <h1 class="mx-2 text-3xl font-bold">{recipe.name}</h1>
+        <button on:click={openRenameDialog}><Icon icon={pencil} class="w-4 h-4 m-1" /></button>
       </div>
-    </div>
-  </Dialog>
+    </Header>
 
-  <main class="h-full">
-    <article class="w-3/4 h-full mx-auto mt-2 prose">
+    <Dialog visible={showRenameDialog} inverted closable={false}>
+      <div class="flex flex-col h-40 p-5">
+        <label for="recipe_name" class="block text-sm font-medium text-gray-700">Name</label>
+        <input
+          id="recipe_name"
+          type="text"
+          bind:value={recipeNameInput}
+          class="mt-0 block w-60 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"
+        />
+        <div class="flex justify-end gap-4 mt-auto">
+          <button class="text-white bg-red-500 dialogBtn hover:bg-red-400" on:click={closeDialog}>Cancel</button>
+          <button
+            class="text-indigo-500 bg-transparent dialogBtn hover:bg-gray-100 hover:text-indigo-400"
+            on:click={saveName}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </Dialog>
+
+    <article class="flex items-stretch flex-grow w-3/4 mx-auto mt-2 prose">
       <Quill
         bind:quill
         initalData={recipe.contents}
@@ -99,5 +99,7 @@
 
   :global(.ql-container) {
     font-size: inherit !important;
+    height: inherit !important;
+    @apply flex-1;
   }
 </style>
