11af0e29f467d0276955cc1dd9fc2c03e37d4ede
Zrean Tofiq <mote93@gmail.com>
2021-03-14 01:41:41 +0100

    enable hmr

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import svelte from 'rollup-plugin-svelte'
+import svelte from 'rollup-plugin-svelte-hot'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
@@ -7,7 +7,9 @@ import css from 'rollup-plugin-css-only'
 import sveltePreprocess from 'svelte-preprocess'
 import json from '@rollup/plugin-json'
 import replace from 'rollup-plugin-replace'
+import Hmr from 'rollup-plugin-hot'
 
+const isNollup = !!process.env.NOLLUP
 const production = !process.env.ROLLUP_WATCH
 
 export default {
@@ -17,41 +19,51 @@ export default {
     format: 'esm',
     name: 'app',
     dir: 'public/build',
+    chunkFileNames: `[name]${(production && '-[hash]') || ''}.js`,
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: (css) => {
-        css.write('bundle.css')
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
       },
       preprocess: sveltePreprocess({
         sourceMap: !production,
         postcss: true,
       }),
+      hot: isNollup,
     }),
     json(),
     replace({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    css({ output: 'public/plugins.css' }),
+    css({ output: 'public/build/bundle.css' }),
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration —
     // consult the documentation for details:
     // https://github.com/rollup/rollup-plugin-commonjs
-    resolve({ browser: true }),
+    resolve({
+      browser: true,
+      dedupe: (importee) => !!importee.match(/svelte(\/|$)/),
+    }),
     commonjs(),
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
-    !production && livereload('public'),
+    !production && !isNollup && livereload('public'),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser(),
+    !production && isNollup && Hmr({ inMemory: true, public: 'public' }), // refresh only updated code
+    {
+      // provide node environment on the client
+      transform: (code) => ({
+        code: code.replace('process.env.NODE_ENV', `"${process.env.NODE_ENV}"`),
+        map: { mappings: '' },
+      }),
+    },
   ],
   watch: {
     clearScreen: false,
