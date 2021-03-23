62c9e6a3644c0cc9502e0cf1697042cfbe2c61a0
Zrean Tofiq <mote93@gmail.com>
2021-03-23 22:31:47 +0100

    add firebase config

---
+++ src/firebase_config.js
@@ -0,0 +1,10 @@
+const firebase_config = {
+  apiKey: 'AIzaSyCsOkutboGIIbyaINBNHmcwVHQnHjCXUGU',
+  authDomain: 'kookbook-ae598.firebaseapp.com',
+  projectId: 'kookbook-ae598',
+  storageBucket: 'kookbook-ae598.appspot.com',
+  messagingSenderId: '654617264719',
+  appId: '1:654617264719:web:babde7b69a85ec09fb603a',
+}
+
+export { firebase_config }
